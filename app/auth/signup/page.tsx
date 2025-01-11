60694ebbe0a43de6510c6466bb0d753d07982694
hunterrr27 <132510702+hunterrr27@users.noreply.github.com>
2025-01-11 18:57:15 -0500

    fixed signup styles

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -24,7 +24,7 @@ export default function SignUp() {
     }
 
     return (
-        <div className='flex-col items-center justify-center min-h-screen bg-gray-100'>
+        <div className='flex items-center justify-center min-h-screen bg-gray-100'>
             
             <motion.div 
                 initial={{ y: 300, opacity: 0 }} 
